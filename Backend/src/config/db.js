ee3fa2a36f90eed2da3212c17d65f69b22067690
NandPatel2477 <koradiyanand2477@gmail.com>
2025-03-11 01:52:52 +0530

    db.js file updated

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,25 +1,35 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
+  host: 'localhost',
+  user: 'root',
+  password: '2477',
+  database: "Sample_DB",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err.message);
-  } else {
-    console.log("Connected to MySQL Database");
-    connection.release();
-  }
-});
+const data = await pool.execute(`
+  SELECT * FROM Students
+  `)
 
-export default pool.promise();
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Database connection failed:", err.message);
+    } else {
+      console.log("Connected to MySQL Database");
+      connection.release();
+    }
+  });
+  
+  console.log(data)
+
+export default pool;
