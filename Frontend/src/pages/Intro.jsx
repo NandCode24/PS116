48f88369850822347d81cf14cd616aef678a9098
RudraViroja <rudraviroja6@gmail.com>
2025-03-12 10:29:01 +0530

    Intro page added

---
+++ Frontend/src/pages/Intro.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../assets/learnsync_no_bg.png'
+
+function Intro() {
+  return (
+    <div>
+        <div className='Intro'>
+            <img src={logo} alt="" width='max' height='500px'/>
+            <div className='para'>
+                <h1><b>ONE STOP DESTINATION FOR QUALITY LEARNING</b></h1>
+
+                <div className='details'>
+                    <p>-Where <b>A Student</b> can learn from individual tutor or his/her respective Institute.</p>
+                    <br/>
+                    <p>-With <b>Pesonalised AI System</b> for clearing Doubts & Questions!</p>
+                    <br/>
+                    <p>-Where <b>A Tutor</b> can teach as an individual.</p>
+                    <br/>
+                    <p>-Where <b>A Parent</b> can see their childs progress.</p>
+                    <br/>
+                    <p>-Where <b>An Institute</b> can create institute and teach according to their curriculum.</p>
+                    <br/>
+                </div>
+
+                <div className='btn'>
+                    <button id='1'>Enroll as Student</button>
+                    <button id='2'>Enroll as Tutor</button>
+                    <button id='3'>Enroll as Parent</button>
+                    <button id='4'>Create Institute</button>
+                </div>
+            </div>
+           
+        </div>
+        
+    </div>
+  )
+}
+
+export default Intro
